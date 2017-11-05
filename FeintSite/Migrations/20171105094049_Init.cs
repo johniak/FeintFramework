8cf13a81be5312ec01a80a2c1b5c7e88cf7f4460
Jan Romaniak <jan.romaniak@stermedia.eu>
2017-11-05 12:03:55 +0100

    db

---
+++ FeintSite/Migrations/20171105094049_Init.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FeintSite.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExampleModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExamplePropertyInteger = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExamplePropertyString = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExampleModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SessionKey",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Key = table.Column<string>(type: "TEXT", nullable: true),
+                    SessionKeyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SessionKey", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SessionProperty",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    OwnerId = table.Column<int>(type: "INTEGER", nullable: true),
+                    SessionPropertyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Value = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SessionProperty", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SessionProperty_SessionKey_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "SessionKey",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SessionProperty_OwnerId",
+                table: "SessionProperty",
+                column: "OwnerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExampleModel");
+
+            migrationBuilder.DropTable(
+                name: "SessionProperty");
+
+            migrationBuilder.DropTable(
+                name: "SessionKey");
+        }
+    }
+}
