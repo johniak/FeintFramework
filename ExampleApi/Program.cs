ab04c35d6428369fea4624fc9aeedf42efb28871
Jan Romaniak <jan.romaniak@stermedia.eu>
2017-11-05 13:18:07 +0100

    Version + example app

---
+++ ExampleApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using FeintApi;
+using FeintSDK;
+using FeintServer.Core;
+using System.Reflection;
+using System.Linq;
+namespace FeintSite
+{
+
+    class Program
+    {
+        public static Response Index(Request request)
+        {
+            var response = new ApiResponse("{\"Hello\":\"World\"}");
+            return response;
+        }
+
+        static void Main(string[] args)
+        {
+            Settings.Urls.Add(new Url(@"^/$", Program.Index));
+            Server s = new Server("0.0.0.0", 5000);
+            s.Start(); 
+        }
+    }
+}
---
+++ ExampleApi/README.md
@@ -0,0 +1,6 @@
+#Getting started
+
+To start web application you will need docker
+
+    docker build -t example_api .
+    docker run -p 5000:5000 example_api
